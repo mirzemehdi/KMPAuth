0c4f229ae8f3f399822f5a60122fbc4fa49e00bc
Mirzamehdi <mirzemehdi@gmail.com>
2024-07-28 11:50:38 +0200

    Wasm target for google module

---
+++ kmpauth-google/src/wasmJsMain/kotlin/com/mmk/kmpauth/google/GoogleAuthUiProviderImpl.kt
@@ -0,0 +1,8 @@
+package com.mmk.kmpauth.google
+
+internal class GoogleAuthUiProviderImpl : GoogleAuthUiProvider {
+    override suspend fun signIn(): GoogleUser? {
+        TODO("Not yet implemented")
+    }
+
+}
