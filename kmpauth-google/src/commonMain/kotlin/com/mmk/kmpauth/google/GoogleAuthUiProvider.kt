12b68da5ee384c6452cf729a5f56a5f543c312c6
Mirzamehdi <mirzemehdi@gmail.com>
2025-08-29 12:56:04 +0200

    Fix GoogleAuth UI Provider

--- kmpauth-google/src/commonMain/kotlin/com/mmk/kmpauth/google/GoogleAuthUiProvider.kt
+++ kmpauth-google/src/commonMain/kotlin/com/mmk/kmpauth/google/GoogleAuthUiProvider.kt
@@ -17,25 +17,43 @@ public interface GoogleAuthUiProvider {
      * @return returns GoogleUser or null(if sign-in was not successful)
      */
     public suspend fun signIn(): GoogleUser? =
-        signIn(filterByAuthorizedAccounts = false, isAutoSelectEnabled = true, scopes = BASIC_AUTH_SCOPE)
-
-    /**
-     * @param filterByAuthorizedAccounts set to true so users can choose between available accounts to sign in.
-     * setting to false list any accounts that have previously been used to sign in to your app.
-     */
-    public suspend fun signIn(filterByAuthorizedAccounts: Boolean, isAutoSelectEnabled: Boolean): GoogleUser? =
-        signIn(filterByAuthorizedAccounts = filterByAuthorizedAccounts, isAutoSelectEnabled = isAutoSelectEnabled, scopes = BASIC_AUTH_SCOPE)
+        signIn(
+            filterByAuthorizedAccounts = false,
+            isAutoSelectEnabled = true,
+            scopes = BASIC_AUTH_SCOPE
+        )
 
 
     /**
      * @param filterByAuthorizedAccounts set to true so users can choose between available accounts to sign in.
      * setting to false list any accounts that have previously been used to sign in to your app. Default value is false.
+     * @param isAutoSelectEnabled If `true`, the user will be automatically signed in
+     * without showing the account chooser when there is exactly one eligible account.
+     * If `false`, the account chooser will be shown even if only one account is available. Default value is true
+     */
+    public suspend fun signIn(
+        filterByAuthorizedAccounts: Boolean,
+        isAutoSelectEnabled: Boolean = true
+    ): GoogleUser? =
+        signIn(
+            filterByAuthorizedAccounts = filterByAuthorizedAccounts,
+            isAutoSelectEnabled = isAutoSelectEnabled,
+            scopes = BASIC_AUTH_SCOPE
+        )
+
+
+    /**
+     * @param filterByAuthorizedAccounts set to true so users can choose between available accounts to sign in.
+     * setting to false list any accounts that have previously been used to sign in to your app. Default value is false.
+     * @param isAutoSelectEnabled If `true`, the user will be automatically signed in
+     * without showing the account chooser when there is exactly one eligible account.
+     * If `false`, the account chooser will be shown even if only one account is available. Default value is true
      * @param scopes Custom scopes to retrieve more information. Default value listOf("email", "profile")
      *
      */
     public suspend fun signIn(
         filterByAuthorizedAccounts: Boolean = false,
-        isAutoSelectEnabled: Boolean,
+        isAutoSelectEnabled: Boolean = true,
         scopes: List<String> = BASIC_AUTH_SCOPE
     ): GoogleUser?
 }
